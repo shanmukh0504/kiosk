8af9bbcf0a72faf8b187e4301d68f84ed96a42db
ManiReddyt <tmanireddy2001@gmail.com>
2024-08-14 14:20:14 +0530

    WIP: integrate wagmi

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,22 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
+import { WagmiProvider } from "wagmi";
+import { config } from "./wagmi/config.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </WagmiProvider>
     </HelmetProvider>
   </StrictMode>,
 );
---
+++ src/wagmi/config.ts
@@ -0,0 +1,14 @@
+import { http, createConfig } from "wagmi";
+import { base, mainnet } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
+
+const projectId = "badd60eb677f972ec3c2454256ccfbc8";
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [injected(), walletConnect({ projectId }), metaMask(), safe()],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+});
