0b28a95a7e083facfdb36703ab9fc164b5f068bd
Jaz Gulati <jaskaran.g15@gmail.com>
2024-10-02 16:09:21 +1000

    fix: transactions padding

--- src/components/sidebar/Transactions.tsx
+++ src/components/sidebar/Transactions.tsx
@@ -3,7 +3,7 @@ import { Transaction } from "./Transaction";
 
 export const Transactions = () => {
     return (
-        <div className="flex flex-col gap-4 bg-white/50 rounded-2xl w-full p-4">
+        <div className="flex flex-col gap-4 bg-white/50 rounded-2xl w-full px-4 pt-4 pb-6">
             <Typography size="h5" weight="bold">
                 Transactions
             </Typography>
