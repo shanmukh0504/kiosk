1024849231046ac368949386925f6502f65775fe
Jaz Gulati <jaskaran.g15@gmail.com>
2024-10-02 16:07:58 +1000

    feat: add transaction status

--- src/components/sidebar/Transaction.tsx
+++ src/components/sidebar/Transaction.tsx
@@ -4,13 +4,21 @@ import { BTC } from "../../constants/constants";
 
 export const Transaction = () => {
     return (
-        <div className="">
+        <div className="flex flex-col gap-1">
             <SwapInfo
                 sendAsset={BTC}
                 receiveAsset={BTC}
                 sendAmount="0.06212967"
                 receiveAmount="0.06212967"
             />
+            <div className="flex justify-between">
+                <Typography size="h5" weight="medium">
+                    Completed
+                </Typography>
+                <Typography size="h5" weight="medium">
+                    4 min ago
+                </Typography>
+            </div>
         </div>
     );
 };
