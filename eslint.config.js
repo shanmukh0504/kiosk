9a20be645f4e477b884d598bc71ecdc85d9c5375
theparthway <parthpsh@gmail.com>
2024-10-28 15:02:01 +0530

    fix eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,39 +3,33 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
 
-export default tseslint.config({
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-  ],
-  files: ["**/*.{ts,tsx}"],
-  ignores: ["dist"],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
-    parserOptions: {
-      project: ["./tsconfig.node.json", "./tsconfig.app.json"],
-      tsconfigRootDir: import.meta.dirname,
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:react-hooks/recommended",
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "react/react-in-jsx-scope": "off",
     },
   },
-  settings: { react: { version: "18.3" } },
-  plugins: {
-    react,
-    "react-hooks": reactHooks,
-    "react-refresh": reactRefresh,
-  },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    ...react.configs.recommended.rules,
-    ...react.configs["jsx-runtime"].rules,
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    "react/prop-types": "off",
-  },
-});
+);
