7be00a2a505a9bc5df23ce1f64e99ec82540eb79
ManiReddyt <tmanireddy2001@gmail.com>
2024-10-28 16:26:22 +0530

    Update @gardenfi/react-hooks to version 0.0.1-beta.77

--- src/hooks/useSwap.tsx
+++ src/hooks/useSwap.tsx
@@ -1,39 +1,39 @@
 import { useEffect } from "react";
 import { swapStore } from "../store/swapStore";
 import { IOType } from "../constants/constants";
-import { assetInfoStore } from "../store/assetInfoStore";
-import { constructOrderPair } from "@gardenfi/core";
-import BigNumber from "bignumber.js";
+// import { assetInfoStore } from "../store/assetInfoStore";
+// import { constructOrderPair } from "@gardenfi/core";
+// import BigNumber from "bignumber.js";
 
 export const useSwap = () => {
   const { inputAmount, outputAmount, inputAsset, outputAsset, setAmount } =
     swapStore();
-  const { strategies } = assetInfoStore();
+  // const { strategies } = assetInfoStore();
 
-  const swapLimits =
-    inputAsset &&
-    outputAsset &&
-    strategies.val &&
-    strategies.val[
-      constructOrderPair(
-        inputAsset.chain,
-        inputAsset.atomicSwapAddress,
-        outputAsset.chain,
-        outputAsset.atomicSwapAddress
-      )
-    ];
-  const minAmount =
-    swapLimits &&
-    inputAsset &&
-    new BigNumber(swapLimits.minAmount)
-      .dividedBy(10 ** inputAsset.decimals)
-      .toNumber();
-  const maxAmount =
-    swapLimits &&
-    inputAsset &&
-    new BigNumber(swapLimits.maxAmount)
-      .dividedBy(10 ** inputAsset.decimals)
-      .toNumber();
+  // const swapLimits =
+  //   inputAsset &&
+  //   outputAsset &&
+  //   strategies.val &&
+  //   strategies.val[
+  //     constructOrderPair(
+  //       inputAsset.chain,
+  //       inputAsset.atomicSwapAddress,
+  //       outputAsset.chain,
+  //       outputAsset.atomicSwapAddress
+  //     )
+  //   ];
+  // const minAmount =
+  //   swapLimits &&
+  //   inputAsset &&
+  //   new BigNumber(swapLimits.minAmount)
+  //     .dividedBy(10 ** inputAsset.decimals)
+  //     .toNumber();
+  // const maxAmount =
+  //   swapLimits &&
+  //   inputAsset &&
+  //   new BigNumber(swapLimits.maxAmount)
+  //     .dividedBy(10 ** inputAsset.decimals)
+  //     .toNumber();
 
   // Reset the input and output amounts when the input or output asset changes.
   useEffect(() => {
