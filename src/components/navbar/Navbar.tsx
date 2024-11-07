682dfec6b27fc4edb45cbd3c7734c25f3b30076c
Pallavi <24.pallavi.2003@gmail.com>
2024-11-07 10:10:18 +0530

    fix: Adjust navbar and routes sidebar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,11 @@
-import { Button, GardenFullLogo, Typography } from "@gardenfi/garden-book";
+import {
+  Button,
+  CloseIcon,
+  GardenFullLogo,
+  MenuIcon,
+  Sidebar,
+  Typography,
+} from "@gardenfi/garden-book";
 import { INTERNAL_ROUTES } from "../../constants/constants";
 import { API } from "../../constants/api";
 import { modalNames, modalStore } from "../../store/modalStore";
@@ -11,6 +18,7 @@ import { OrderActions, parseActionFromStatus } from "@gardenfi/core";
 export const Navbar = () => {
   const [isInitiatingSM, setIsInitiatingSM] = useState(false);
   const [shouldInitiateSM, setShouldInitiateSM] = useState(false);
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
 
   const { isConnected } = useEVMWallet();
   const { setOpenModal } = modalStore();
@@ -45,6 +53,10 @@ export const Navbar = () => {
     setIsInitiatingSM(false);
   }, [initializeSecretManager, secretManager]);
 
+  const handleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
   useEffect(() => {
     if (
       !pendingOrders ||
@@ -70,24 +82,26 @@ export const Navbar = () => {
   ]);
 
   return (
-    <div className={"flex items-center px-10 py-6 gap-16"}>
-      <GardenFullLogo
-        onClick={handleHomeLogoClick}
-        className="cursor-pointer"
-      />
-      <div className="flex gap-12">
-        {Object.values(INTERNAL_ROUTES).map((route) => {
-          return (
-            <a key={route.path} href={route.path}>
-              <Typography
-                size="h2"
-                weight={isCurrentRoute(route.path) ? "bold" : "medium"}
-              >
-                {route.name}
-              </Typography>
-            </a>
-          );
-        })}
+    <div className={"flex items-center justify-between px-10 py-6 gap-3"}>
+      <div className="flex items-center gap-16">
+        <GardenFullLogo
+          onClick={handleHomeLogoClick}
+          className="cursor-pointer "
+        />
+        <div className="hidden sm:flex  sm:items-center gap-12">
+          {Object.values(INTERNAL_ROUTES).map((route) => {
+            return (
+              <a key={route.path} href={route.path}>
+                <Typography
+                  size="h2"
+                  weight={isCurrentRoute(route.path) ? "bold" : "medium"}
+                >
+                  {route.name}
+                </Typography>
+              </a>
+            );
+          })}
+        </div>
       </div>
       {isFullyConnected ? (
         <Address />
@@ -100,6 +114,38 @@ export const Navbar = () => {
           Connect
         </Button>
       )}
+      <div
+        className="sm:hidden justify-center flex items-center min-h-8 min-w-8 rounded-full bg-white/50"
+        onClick={handleSidebar}
+      >
+        <MenuIcon />
+      </div>
+
+      <Sidebar open={isSidebarOpen} className="z-40 !px-0 py-6 " size={"small"}>
+        <div className=" flex flex-col items-end py-2 gap-8">
+          <div className="px-6">
+            <CloseIcon onClick={handleSidebar} />
+          </div>
+          <div className={`flex flex-col text-right w-full`}>
+            {Object.values(INTERNAL_ROUTES).map((route) => {
+              return (
+                <a
+                  key={route.path}
+                  href={route.path}
+                  className={`px-6 py-[14px] hover:bg-white`}
+                >
+                  <Typography
+                    size="h2"
+                    weight={isCurrentRoute(route.path) ? "bold" : "medium"}
+                  >
+                    {route.name}
+                  </Typography>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+      </Sidebar>
     </div>
   );
 };
