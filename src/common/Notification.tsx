1644a71d124cd0f5932da564f17ddda3e881219c
Pallavi <24.pallavi.2003@gmail.com>
2024-11-08 18:47:54 +0530

    fix : Add padding to wallet icon

--- src/common/Notification.tsx
+++ src/common/Notification.tsx
@@ -19,7 +19,7 @@ export const Notification: FC<NotificationProps> = ({
   return (
     <div
       className={`bg-white/50 backdrop-blur-[20px]
-            fixed left-10 bottom-10 p-4
+            fixed left-10 bottom-10 p-4 z-[9999]
             transition-[border-radius,width,height,transform] ease-cubic-in-out duration-300
             ${
               visible
