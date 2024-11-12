52c0b81529abb788443184f99ff1cc0f1be80b5c
Reetik Rajan <r4reetik@users.noreply.github.com>
2024-11-12 08:59:05 +0700

    chore: simplify checkIfWhitelisted function to always return true

--- src/utils/checkIfWhitelisted.ts
+++ src/utils/checkIfWhitelisted.ts
@@ -2,12 +2,5 @@ import axios from "axios";
 import { API } from "../constants/api";
 
 export const checkIfWhitelisted = async (address: string) => {
-  try {
-    const res = await axios.get<{
-      is_whitelisted: boolean;
-    }>(API().whitelist(address.toLowerCase()));
-    return res.data.is_whitelisted;
-  } catch {
-    return false;
-  }
+  return true;
 };
