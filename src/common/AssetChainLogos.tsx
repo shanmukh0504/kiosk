40bd3e43c439465227cb6a05ddffeaa0f5a8a745
panugothrakesh <rakeshpanugoth2003@gmail.com>
2024-12-13 16:39:26 +0530

    Chroes: Added Esc Button Listner to close Transactions Sidebar and Fixed the AssetChainLogo Alignment

--- src/common/AssetChainLogos.tsx
+++ src/common/AssetChainLogos.tsx
@@ -10,10 +10,10 @@ export const AssetChainLogos: FC<AssetChainLogosProps> = ({
   chainLogo,
 }) => {
   return (
-    <div className="flex">
-      <img src={tokenLogo} className="w-5 h-5 z-30" />
+    <div className={`flex ${chainLogo ? "" : "-translate-x-1"}`}>
+      <img src={tokenLogo} className={`w-5 h-5 z-30 ${chainLogo ? "translate-x-1" : ""}`} />
       {chainLogo ? (
-        <img src={chainLogo} className="w-5 h-5 z-20 -translate-x-1" />
+        <img src={chainLogo} className="w-5 h-5 z-20" />
       ) : null}
     </div>
   );
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { modalNames, modalStore } from "../../store/modalStore";
 import { AssetList } from "./AssetList";
 import { ConnectWallet } from "../navbar/connectWallet/ConnectWallet";
@@ -10,6 +11,22 @@ export const Modal = () => {
   const { modalName, setCloseModal } = modalStore();
   const { isOpen, isBTCwallets, closeConnectWallet } = connectWalletStore();
 
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setCloseModal(modalNames.transactions);
+      }
+    };
+
+    if (modalName.transactions) {
+      window.addEventListener("keydown", handleKeyDown);
+    }
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [modalName.transactions]);
+
   return (
     <>
       <ConnectWallet
@@ -17,6 +34,7 @@ export const Modal = () => {
         onClose={() => closeConnectWallet()}
         isBTCWallets={isBTCwallets}
       />
+      {/* Esc */}
       <TransactionsComponent
         open={modalName.transactions}
         onClose={() => setCloseModal(modalNames.transactions)}
