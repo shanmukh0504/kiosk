f169c8043734db97f86150a6f3b4e0e7b858074e
ManiReddyt <tmanireddy2001@gmail.com>
2024-12-16 17:20:50 +0530

    style: add rounded corners to asset chain logos in AssetChainLogos component

--- src/common/AssetChainLogos.tsx
+++ src/common/AssetChainLogos.tsx
@@ -10,10 +10,20 @@ export const AssetChainLogos: FC<AssetChainLogosProps> = ({
   chainLogo,
 }) => {
   return (
-    <div className={`relative flex items-center justify-between h-5 ${chainLogo ? "w-[36px]" : "w-5"}`}>
-      <img src={tokenLogo} className="absolute left-0 w-5 h-5 z-30" />
+    <div
+      className={`relative flex items-center justify-between h-5 ${
+        chainLogo ? "w-[36px]" : "w-5"
+      }`}
+    >
+      <img
+        src={tokenLogo}
+        className="absolute left-0 w-5 h-5 z-30 rounded-full"
+      />
       {chainLogo ? (
-        <img src={chainLogo} className="absolute right-0 w-5 h-5 z-20" />
+        <img
+          src={chainLogo}
+          className="absolute right-0 w-5 h-5 z-20 rounded-full"
+        />
       ) : null}
     </div>
   );
