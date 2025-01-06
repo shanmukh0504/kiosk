767f62eb781b4169df6362c33ba97cd70b7dfb14
ManiReddyt <tmanireddy2001@gmail.com>
2025-01-06 16:46:18 +0530

    fix/build issues

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -4,7 +4,6 @@ import { AssetList } from "./AssetList";
 import { ConnectWallet } from "../navbar/connectWallet/ConnectWallet";
 import { TransactionsComponent } from "../transactions/TransactionsComponent";
 import { WhiteList } from "../whitelist/WhiteList";
-import { InitializeSMModal } from "./InitializeSMModal";
 import { connectWalletStore } from "../../store/connectWalletStore";
 
 export const Modal = () => {
@@ -35,10 +34,6 @@ export const Modal = () => {
       <TransactionsComponent
         open={modalName.transactions}
         onClose={() => setCloseModal(modalNames.transactions)}
-      />
-      <InitializeSMModal
-        open={modalName.initializeSM}
-        onClose={() => setCloseModal(modalNames.initializeSM)}
       />
       <WhiteList
         open={modalName.whiteList}
