aad2ecc789c9f84b1f7ba491d198ca770b76a738
ManiReddyt <tmanireddy2001@gmail.com>
2025-01-08 11:39:53 +0530

    fix: build

--- src/components/stake/StakeInput.tsx
+++ src/components/stake/StakeInput.tsx
@@ -4,61 +4,63 @@ import { stakeStore } from "../../store/stakeStore";
 import { useBalances } from "../../hooks/useBalances";
 
 export const StakeInput = () => {
-    const { asset, inputAmount, setInputAmount } = stakeStore();
-    const balances = useBalances(asset);
-    const balance = balances.inputTokenBalance
-        ? balances.inputTokenBalance.toFixed(3)
-        : "0.000";
+  const { asset, inputAmount, setInputAmount } = stakeStore();
+  const balances = useBalances(asset);
+  const balance = balances.tokenBalance
+    ? balances.tokenBalance.toFixed(3)
+    : "0.000";
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setInputAmount(e.target.value);
-    };
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputAmount(e.target.value);
+  };
 
-    const handleBalanceClick = () => {
-        if (!balance || balance === "0.000") {
-            return;
-        }
-        setInputAmount(balance);
-    };
+  const handleBalanceClick = () => {
+    if (!balance || balance === "0.000") {
+      return;
+    }
+    setInputAmount(balance);
+  };
 
-    const handleMinusClick = () => {
-        const newAmount = Math.max(Number(inputAmount) - 1, 0);
-        setInputAmount(newAmount.toString());
-    };
+  const handleMinusClick = () => {
+    const newAmount = Math.max(Number(inputAmount) - 1, 0);
+    setInputAmount(newAmount.toString());
+  };
 
-    const handlePlusClick = () => {
-        const newAmount = Number(inputAmount) + 1;
-        setInputAmount(newAmount.toString());
-    };
+  const handlePlusClick = () => {
+    const newAmount = Number(inputAmount) + 1;
+    setInputAmount(newAmount.toString());
+  };
 
-    return (
-        <div className="flex flex-col gap-3 p-4 bg-white rounded-xl">
-            <div className="flex justify-between">
-                <Typography size="h5" weight="bold">Stake SEED</Typography>
-                <Typography
-                    size="h5"
-                    weight="bold"
-                    className="cursor-pointer"
-                    onClick={handleBalanceClick}
-                >
-                    {balance} available
-                </Typography>
-            </div>
-            <div className="flex justify-between">
-                <Typography size={"h2"} weight="bold">
-                    <input
-                        className="max-w-[200px] outline-none placeholder:text-mid-grey"
-                        type="text"
-                        value={inputAmount}
-                        placeholder="0"
-                        onChange={handleInputChange}
-                    />
-                </Typography>
-                <div className="flex gap-3">
-                    <MinusIcon onClick={handleMinusClick} />
-                    <PlusIcon onClick={handlePlusClick} />
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col gap-3 p-4 bg-white rounded-xl">
+      <div className="flex justify-between">
+        <Typography size="h5" weight="bold">
+          Stake SEED
+        </Typography>
+        <Typography
+          size="h5"
+          weight="bold"
+          className="cursor-pointer"
+          onClick={handleBalanceClick}
+        >
+          {balance} available
+        </Typography>
+      </div>
+      <div className="flex justify-between">
+        <Typography size={"h2"} weight="bold">
+          <input
+            className="max-w-[200px] outline-none placeholder:text-mid-grey"
+            type="text"
+            value={inputAmount}
+            placeholder="0"
+            onChange={handleInputChange}
+          />
+        </Typography>
+        <div className="flex gap-3">
+          <MinusIcon onClick={handleMinusClick} />
+          <PlusIcon onClick={handlePlusClick} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
