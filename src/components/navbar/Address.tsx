dee4d37c16c9687976bcc317c4934e9d41d4d68b
panugothrakesh <rakeshpanugoth2003@gmail.com>
2025-01-28 16:32:17 +0530

    Chores: Address in progress number alignment

--- src/components/navbar/Address.tsx
+++ src/components/navbar/Address.tsx
@@ -36,7 +36,7 @@ export const Address = () => {
       {pendingOrdersFromStore?.length ? (
         <div className="relative">
           <Loader />
-          <div className="absolute text-rose text-sm font-bold top-[10%] w-full items-center text-center">
+          <div className="absolute text-rose text-sm font-bold top-[8%] w-full items-center text-center">
             {pendingOrdersFromStore.length}
           </div>
         </div>
