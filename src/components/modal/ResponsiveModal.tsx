f2d259831d1bf885cef89a2180f523cb5009e50c
panugothrakesh <rakeshpanugoth2003@gmail.com>
2025-04-09 17:27:44 +0530

    feat: add version update modal and handle MetaMask version check

--- src/components/modal/ResponsiveModal.tsx
+++ src/components/modal/ResponsiveModal.tsx
@@ -4,11 +4,21 @@ import { FC, ReactNode } from "react";
 import { ModalProps } from "./Modal";
 import { viewPortStore } from "../../store/viewPortStore";
 
+export type OpacityLevel =
+  | "light"
+  | "medium"
+  | "semi-dark"
+  | "extra-light"
+  | "full"
+  | null
+  | undefined;
+
 export const ResponsiveModal: FC<
   ModalProps & {
     children: ReactNode;
+    opacityLevel?: OpacityLevel;
   }
-> = ({ open, onClose, children }) => {
+> = ({ open, onClose, children, opacityLevel = "medium" }) => {
   const { isMobile } = viewPortStore();
 
   return (
@@ -20,7 +30,7 @@ export const ResponsiveModal: FC<
       ) : (
         <Modal open={open} onClose={onClose}>
           <Modal.Children
-            opacityLevel={"medium"}
+            opacityLevel={opacityLevel}
             className="flex max-w-[600px] flex-col gap-6 rounded-2xl p-3"
           >
             {children}
