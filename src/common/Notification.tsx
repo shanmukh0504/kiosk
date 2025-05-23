ee4141991df013c73be33b78ab39a2de9deb3b1b
SATHVIK SVS <svssathvik77@gmail.com>
2025-05-23 18:23:11 +0530

    handle no notification case

--- src/common/Notification.tsx
+++ src/common/Notification.tsx
@@ -49,38 +49,38 @@ export const Notification = () => {
   };
 
   return (
-    notification && (
+    <div
+      className={`fixed bottom-10 left-2 right-2 z-40 bg-white/50 p-4 backdrop-blur-[20px] transition-[border-radius,width,height,transform] duration-300 ease-cubic-in-out sm:bottom-10 sm:left-10 ${
+        visible
+          ? "h-24 w-[344px] rounded-2xl sm:w-[460px]"
+          : "h-12 w-12 cursor-pointer rounded-3xl hover:scale-105"
+      }`}
+      onClick={() => notification && !visible && setVisible(true)}
+    >
+      {/* TODO: Replace with bell icon once added to garden-book */}
       <div
-        className={`fixed bottom-10 left-2 right-2 z-40 bg-white/50 p-4 backdrop-blur-[20px] transition-[border-radius,width,height,transform] duration-300 ease-cubic-in-out sm:bottom-10 sm:left-10 ${
+        className={`flex items-center justify-center transition-[opacity,height,width] ease-cubic-in-out ${
           visible
-            ? "h-24 w-[344px] rounded-2xl sm:w-[460px]"
-            : "h-12 w-12 cursor-pointer rounded-3xl hover:scale-105"
+            ? // On open, fade out the bell icon
+              "h-0 opacity-0"
+            : // On close, fade in the bell icon once the notification has collapsed
+              "h-full opacity-100 delay-300"
         }`}
-        onClick={() => !visible && setVisible(true)}
       >
-        {/* TODO: Replace with bell icon once added to garden-book */}
-        <div
-          className={`flex items-center justify-center transition-[opacity,height,width] ease-cubic-in-out ${
-            visible
-              ? // On open, fade out the bell icon
-                "h-0 opacity-0"
-              : // On close, fade in the bell icon once the notification has collapsed
-                "h-full opacity-100 delay-300"
-          }`}
+        <svg
+          width="16"
+          height="20"
+          viewBox="0 0 16 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            width="16"
-            height="20"
-            viewBox="0 0 16 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M0 17V15H2V8C2 6.61667 2.41667 5.3875 3.25 4.3125C4.08333 3.2375 5.16667 2.53333 6.5 2.2V1.5C6.5 1.08333 6.64583 0.729167 6.9375 0.4375C7.22917 0.145833 7.58333 0 8 0C8.41667 0 8.77083 0.145833 9.0625 0.4375C9.35417 0.729167 9.5 1.08333 9.5 1.5V2.2C10.8333 2.53333 11.9167 3.2375 12.75 4.3125C13.5833 5.3875 14 6.61667 14 8V15H16V17H0ZM8 20C7.45 20 6.97917 19.8042 6.5875 19.4125C6.19583 19.0208 6 18.55 6 18H10C10 18.55 9.80417 19.0208 9.4125 19.4125C9.02083 19.8042 8.55 20 8 20ZM4 15H12V8C12 6.9 11.6083 5.95833 10.825 5.175C10.0417 4.39167 9.1 4 8 4C6.9 4 5.95833 4.39167 5.175 5.175C4.39167 5.95833 4 6.9 4 8V15Z"
-              fill="#554B6A"
-            />
-          </svg>
-        </div>
+          <path
+            d="M0 17V15H2V8C2 6.61667 2.41667 5.3875 3.25 4.3125C4.08333 3.2375 5.16667 2.53333 6.5 2.2V1.5C6.5 1.08333 6.64583 0.729167 6.9375 0.4375C7.22917 0.145833 7.58333 0 8 0C8.41667 0 8.77083 0.145833 9.0625 0.4375C9.35417 0.729167 9.5 1.08333 9.5 1.5V2.2C10.8333 2.53333 11.9167 3.2375 12.75 4.3125C13.5833 5.3875 14 6.61667 14 8V15H16V17H0ZM8 20C7.45 20 6.97917 19.8042 6.5875 19.4125C6.19583 19.0208 6 18.55 6 18H10C10 18.55 9.80417 19.0208 9.4125 19.4125C9.02083 19.8042 8.55 20 8 20ZM4 15H12V8C12 6.9 11.6083 5.95833 10.825 5.175C10.0417 4.39167 9.1 4 8 4C6.9 4 5.95833 4.39167 5.175 5.175C4.39167 5.95833 4 6.9 4 8V15Z"
+            fill="#554B6A"
+          />
+        </svg>
+      </div>
+      {notification && (
         <div
           className={`flex justify-between gap-3 ${
             visible
@@ -118,7 +118,7 @@ export const Notification = () => {
             </div>
           </div>
         </div>
-      </div>
-    )
+      )}
+    </div>
   );
 };
