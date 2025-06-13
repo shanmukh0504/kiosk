b08166acd19fa3ce33658fc0e44211a3fe3120d1
Pallavi <24.pallavi.2003@gmail.com>
2025-06-13 15:13:40 +0530

    fix: display protocol fee as 0 when fees are not present

--- src/components/swap/FeesAndRateDetails.tsx
+++ src/components/swap/FeesAndRateDetails.tsx
@@ -204,7 +204,7 @@ export const FeesAndRateDetails = () => {
                 </Typography>
                 <div className="flex gap-5">
                   <Typography size="h4" weight="medium">
-                    {fees ? "$" + protocolFee : ""}
+                    {fees ? "$" + protocolFee || 0 : ""}
                   </Typography>
                 </div>
               </div>
