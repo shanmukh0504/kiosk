623b0f2e8f5ad7f145c198ec132495c5c42bc430
Pallavi <24.pallavi.2003@gmail.com>
2025-07-11 12:09:45 +0530

    chore: update fee breakdown ui

--- src/components/swap/FeesAndRateDetails.tsx
+++ src/components/swap/FeesAndRateDetails.tsx
@@ -1,33 +1,33 @@
-import { useState, useMemo, useRef } from "react";
-import { InfoIcon, KeyboardDownIcon, Typography } from "@gardenfi/garden-book";
+import { useState, useMemo } from "react";
+import {
+  KeyboardDownIcon,
+  SwapHorizontalIcon,
+  Typography,
+} from "@gardenfi/garden-book";
 import { BTC, swapStore } from "../../store/swapStore";
 import { CompetitorComparisons } from "./CompetitorComparisons";
 import { useBitcoinWallet } from "@gardenfi/wallet-connectors";
 import { useEVMWallet } from "../../hooks/useEVMWallet";
 import { isBitcoin, isSolana } from "@gardenfi/orderbook";
 import { motion, AnimatePresence } from "framer-motion";
-import { formatAmount, getProtocolFee } from "../../utils/utils";
+import { getProtocolFee } from "../../utils/utils";
 import { getBitcoinNetwork } from "../../constants/constants";
 import { useNetworkFees } from "../../hooks/useNetworkFees";
 import {
   fadeAnimation,
   delayedFadeAnimation,
 } from "../../animations/animations";
-import { TooltipWrapper } from "../../common/ToolTipWrapper";
-import { CostToolTip } from "./CostToolTip";
 import { SwapSavingsAndAddresses } from "./SwapSavingsAndAddresses";
 import { useSolanaWallet } from "../../hooks/useSolanaWallet";
 
 export const FeesAndRateDetails = () => {
   const [isDetailsExpanded, setIsDetailsExpanded] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
   const [showComparison, setIsShowComparison] = useState({
     isTime: false,
     isFees: false,
   });
   const [maxTimeSaved, setMaxTimeSaved] = useState<number>(0);
   const [maxCostSaved, setMaxCostSaved] = useState<number>(0);
-  const targetRef = useRef<HTMLDivElement>(null);
 
   const { tokenPrices, inputAsset, outputAsset, rate, setIsComparisonVisible } =
     swapStore();
@@ -43,14 +43,6 @@ export const FeesAndRateDetails = () => {
   );
 
   const protocolFee = useMemo(() => getProtocolFee(fees), [fees]);
-  const totalCost = protocolFee + networkFeesValue;
-
-  const priceImpact = useMemo(() => {
-    if (!tokenPrices) return 0;
-    const input = Number(tokenPrices.input);
-    const output = Number(tokenPrices.output);
-    return (1 - (output + protocolFee) / input) * 100;
-  }, [tokenPrices, protocolFee]);
 
   const isBitcoinChains = outputAsset?.symbol.includes(BTC.symbol);
   const formattedRate = useMemo(
@@ -116,55 +108,36 @@ export const FeesAndRateDetails = () => {
           <div className="relative flex w-full items-center justify-start">
             <AnimatePresence mode="wait">
               <motion.div
-                key="rate"
+                key="label-rate"
                 {...fadeAnimation}
                 className="absolute left-0 flex items-center justify-start gap-1"
               >
                 <Typography
                   size="h5"
                   weight="medium"
-                  className="!flex !text-mid-grey"
+                  className="!text-mid-grey"
                 >
-                  Total cost
+                  1 {inputAsset?.symbol}
                 </Typography>
-                <span
-                  ref={targetRef}
-                  className="inline-block cursor-pointer"
-                  onMouseEnter={() => setIsHovered(true)}
-                  onMouseLeave={() => setIsHovered(false)}
-                >
-                  <InfoIcon className="h-3 w-3 !fill-mid-grey" />
-                  {isHovered && inputAsset && outputAsset && (
-                    <TooltipWrapper targetRef={targetRef}>
-                      <CostToolTip
-                        networkFee={networkFeesValue}
-                        protocolFee={protocolFee}
-                        rate={formattedRate}
-                        priceImpact={priceImpact}
-                      />
-                    </TooltipWrapper>
-                  )}
-                </span>
+                <SwapHorizontalIcon className="h-3 w-3" />
               </motion.div>
             </AnimatePresence>
           </div>
-          <div className="flex items-center gap-1">
+          <div className="flex w-full items-center justify-end gap-1">
             <AnimatePresence mode="wait">
               <motion.div
-                key="fees-content"
+                key="value-rate"
                 {...delayedFadeAnimation}
                 className="flex items-center gap-1"
               >
                 <Typography size="h5" weight="medium">
-                  ${formatAmount(totalCost, 0, 2)}
+                  {formattedRate} {outputAsset?.symbol}
                 </Typography>
               </motion.div>
             </AnimatePresence>
+
             <AnimatePresence>
-              {(maxTimeSaved > 0 ||
-                maxCostSaved > 0 ||
-                receiveAddress ||
-                refundAddress) && (
+              {
                 <motion.div
                   initial={{ width: 0, scale: 0, opacity: 0 }}
                   animate={{ width: "auto", scale: 1, opacity: 1 }}
@@ -178,7 +151,7 @@ export const FeesAndRateDetails = () => {
                     onClick={() => setIsDetailsExpanded(!isDetailsExpanded)}
                   />
                 </motion.div>
-              )}
+              }
             </AnimatePresence>
           </div>
         </div>
@@ -190,6 +163,8 @@ export const FeesAndRateDetails = () => {
               refundAddress={refundAddress}
               receiveAddress={receiveAddress}
               showComparison={handleShowComparison}
+              protocolFee={protocolFee}
+              networkFeesValue={networkFeesValue}
             />
           )}
         </AnimatePresence>
