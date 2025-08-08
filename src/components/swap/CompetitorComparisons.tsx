65b0cd77597c3be131a75092acc35a50113bbf00
pallavi <24.pallavi.2003@gmail.com>
2025-08-08 10:27:27 +0530

    fix: competitor comparisons UI

--- src/components/swap/CompetitorComparisons.tsx
+++ src/components/swap/CompetitorComparisons.tsx
@@ -6,7 +6,7 @@ import {
   formatTime,
   formatTimeDiff,
 } from "../../utils/timeAndFeeComparison/utils";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { formatAmount } from "../../utils/utils";
 import BigNumber from "bignumber.js";
 import { useCompetitorTimeFees } from "../../hooks/useCompetitorTimeFees";
@@ -75,106 +75,108 @@ export const CompetitorComparisons: FC<CompetitorComparisonsProps> = ({
   }, [hide]);
 
   return (
-    <motion.div
-      {...animationConfig}
-      className="absolute left-0 top-0 z-30 flex h-full w-full flex-col gap-3 rounded-[20px] bg-primary-lighter p-3"
-    >
-      <div className="flex items-center justify-between p-1">
-        <Typography size="h4" weight="medium">
-          {isTime ? "Time saved" : isFees ? "Cost saved" : "Saved"}
-        </Typography>
-        <ArrowLeftIcon className="cursor-pointer" onClick={hide} />
-      </div>
+    <AnimatePresence>
+      <motion.div
+        {...animationConfig}
+        className="bg-opacity/50 absolute left-0 top-0 z-30 flex h-full w-full flex-col gap-3 rounded-[20px] p-3"
+      >
+        <div className="flex items-center justify-between p-1">
+          <Typography size="h4" weight="medium">
+            {isTime ? "Time saved" : isFees ? "Cost saved" : "Saved"}
+          </Typography>
+          <ArrowLeftIcon className="cursor-pointer" onClick={hide} />
+        </div>
 
-      {inputAsset &&
-        outputAsset &&
-        inputAmountInDecimals &&
-        outputAmountInDecimals && (
-          <div className="flex flex-col gap-2 rounded-2xl bg-white p-4">
-            <SwapInfo
-              equalSplit={true}
-              sendAsset={inputAsset}
-              receiveAsset={outputAsset}
-              sendAmount={formatAmount(
-                inputAmountInDecimals,
-                inputAsset.decimals,
-                Math.min(inputAsset.decimals, BTC.decimals)
-              )}
-              receiveAmount={formatAmount(
-                outputAmountInDecimals,
-                outputAsset.decimals,
-                Math.min(outputAsset.decimals, BTC.decimals)
-              )}
-            />
-          </div>
-        )}
-
-      <div className="flex h-full flex-col gap-3 rounded-2xl bg-white p-4">
-        {swapEntriesWithGarden.map(([key, { fee, time }], index) => {
-          const source = swapSources.find((s) => s.key === key);
-          const feeDiff = (Number(fee) - gardenFee).toFixed(2);
-
-          return (
-            <div key={key} className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                <Typography
-                  size="h5"
-                  weight="regular"
-                  className="mr-2 w-[20px]"
-                >
-                  #{index + 1}
-                </Typography>
-                <div className="flex h-4 w-4 items-center justify-center">
-                  {source && source.icon}
-                </div>
-                <Typography size="h5" weight="regular">
-                  {source?.name || key}
-                </Typography>
-              </div>
-              {isTime && (
-                <div className="flex gap-6">
-                  {key !== "garden" && (
-                    <Typography
-                      className="!text-rose"
-                      size="h5"
-                      weight="regular"
-                    >
-                      {formatTimeDiff(time, gardenSwapTime)}
-                    </Typography>
-                  )}
-                  <Typography
-                    className="!flex !w-16 !justify-end"
-                    size="h5"
-                    weight="regular"
-                  >
-                    {formatTime(time)}
-                  </Typography>
-                </div>
-              )}
-              {isFees && (
-                <div className="flex gap-6">
-                  {key !== "garden" && (
-                    <Typography
-                      className="!text-rose"
-                      size="h5"
-                      weight="regular"
-                    >
-                      {`${Number(feeDiff) >= 0 ? "-" : "+"}$${Math.abs(Number(feeDiff))}`}
-                    </Typography>
-                  )}
-                  <Typography
-                    className="!flex !w-12 !justify-end"
-                    size="h5"
-                    weight="regular"
-                  >
-                    {`$${formatAmount(fee, 0, 2)}`}
-                  </Typography>
-                </div>
-              )}
+        {inputAsset &&
+          outputAsset &&
+          inputAmountInDecimals &&
+          outputAmountInDecimals && (
+            <div className="flex flex-col gap-2 rounded-2xl bg-white p-4">
+              <SwapInfo
+                equalSplit={true}
+                sendAsset={inputAsset}
+                receiveAsset={outputAsset}
+                sendAmount={formatAmount(
+                  inputAmountInDecimals,
+                  inputAsset.decimals,
+                  Math.min(inputAsset.decimals, BTC.decimals)
+                )}
+                receiveAmount={formatAmount(
+                  outputAmountInDecimals,
+                  outputAsset.decimals,
+                  Math.min(outputAsset.decimals, BTC.decimals)
+                )}
+              />
             </div>
-          );
-        })}
-      </div>
-    </motion.div>
+          )}
+
+        <div className="flex h-full flex-col gap-3 rounded-2xl bg-white p-4">
+          {swapEntriesWithGarden.map(([key, { fee, time }], index) => {
+            const source = swapSources.find((s) => s.key === key);
+            const feeDiff = (Number(fee) - gardenFee).toFixed(2);
+
+            return (
+              <div key={key} className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <Typography
+                    size="h5"
+                    weight="regular"
+                    className="mr-2 w-[20px]"
+                  >
+                    #{index + 1}
+                  </Typography>
+                  <div className="flex h-4 w-4 items-center justify-center">
+                    {source && source.icon}
+                  </div>
+                  <Typography size="h5" weight="regular">
+                    {source?.name || key}
+                  </Typography>
+                </div>
+                {isTime && (
+                  <div className="flex gap-6">
+                    {key !== "garden" && (
+                      <Typography
+                        className="!text-rose"
+                        size="h5"
+                        weight="regular"
+                      >
+                        {formatTimeDiff(time, gardenSwapTime)}
+                      </Typography>
+                    )}
+                    <Typography
+                      className="!flex !w-16 !justify-end"
+                      size="h5"
+                      weight="regular"
+                    >
+                      ~{formatTime(time)}
+                    </Typography>
+                  </div>
+                )}
+                {isFees && (
+                  <div className="flex gap-6">
+                    {key !== "garden" && (
+                      <Typography
+                        className="!text-rose"
+                        size="h5"
+                        weight="regular"
+                      >
+                        {`${Number(feeDiff) >= 0 ? "-" : "+"}$${Math.abs(Number(feeDiff))}`}
+                      </Typography>
+                    )}
+                    <Typography
+                      className="!flex !w-12 !justify-end"
+                      size="h5"
+                      weight="regular"
+                    >
+                      {`$${formatAmount(fee, 0, 2)}`}
+                    </Typography>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
