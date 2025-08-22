740865e1e17f99155551425b88588432e28b218d
SaiAdithya3 <saiadithyakancharla@gmail.com>
2025-08-22 12:22:56 +0530

    chore: changed 1 to next

--- src/layout/WalletProviders.tsx
+++ src/layout/WalletProviders.tsx
@@ -11,7 +11,7 @@ import {
   connectors as starknetConnectors,
 } from "./starknet/config";
 import { config } from "./wagmi/config";
-import { MiniAppProvider } from "./MiniAppContextProvider.tsx";
+// import { MiniAppProvider } from "./MiniAppContextProvider.tsx";
 import { STARKNET_CONFIG } from "@gardenfi/core";
 import { SuiProvider } from "./sui/SuiProvider.tsx";
 import { FrameProvider } from "./FrameProvider.tsx";
@@ -22,23 +22,23 @@ interface WalletProviderProps {
 export const WalletProviders: FC<WalletProviderProps> = ({ children }) => {
   return (
     <FrameProvider>
-      <MiniAppProvider>
-        <WagmiProvider config={config}>
-          <BTCWalletProvider network={network as Network} store={localStorage}>
-            <StarknetConfig
-              defaultChainId={BigInt(STARKNET_CONFIG[network].chainId)}
-              chains={starknetChains}
-              provider={starknetProviders}
-              connectors={starknetConnectors}
-              autoConnect
-            >
-              <SolanaProvider>
-                <SuiProvider>{children}</SuiProvider>
-              </SolanaProvider>
-            </StarknetConfig>
-          </BTCWalletProvider>
-        </WagmiProvider>
-      </MiniAppProvider>
+      {/* <MiniAppProvider> */}
+      <WagmiProvider config={config}>
+        <BTCWalletProvider network={network as Network} store={localStorage}>
+          <StarknetConfig
+            defaultChainId={BigInt(STARKNET_CONFIG[network].chainId)}
+            chains={starknetChains}
+            provider={starknetProviders}
+            connectors={starknetConnectors}
+            autoConnect
+          >
+            <SolanaProvider>
+              <SuiProvider>{children}</SuiProvider>
+            </SolanaProvider>
+          </StarknetConfig>
+        </BTCWalletProvider>
+      </WagmiProvider>
+      {/* </MiniAppProvider> */}
     </FrameProvider>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,7 @@ import { rpcStore } from "../store/rpcStore";
 import { useEffect } from "react";
 // import { useMiniKit } from "@coinbase/onchainkit/minikit";
 import { useSuiWallet } from "../hooks/useSuiWallet";
+import sdk from "@farcaster/miniapp-sdk";
 
 function App() {
   const { data: walletClient } = useWalletClient();
@@ -27,9 +28,12 @@ function App() {
   // const { isFrameReady, setFrameReady } = useMiniKit();
 
   useEffect(() => {
+    (async () => {
+      await sdk.actions.ready({ disableNativeGestures: true });
+      await sdk.back.enableWebNavigation();
+    })();
     fetchAndSetRPCs();
   }, []);
-
   // useEffect(() => {
   //   if (!isFrameReady) {
   //     setFrameReady();
